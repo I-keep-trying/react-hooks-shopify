bd74c89ac3af6bac13fb4acfd1f433a4abe96c21
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2019-12-11 11:00:14 -0500

    functional and class components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,61 @@
-import React, { Component } from 'react';
-import './App.css';
-import { connect } from 'react-redux';
-import Cart from './components/shopify/Cart';
-import store from './store';
+import React, { Component } from 'react'
+import './App.css'
+import { connect } from 'react-redux'
+import Cart from './components/shopify/Cart'
+import store from './store'
 
 // custom components
-import Nav from './components/Nav';
-import GenericProductsPage from './components/GenericProductsPage';
+import Nav from './components/Nav'
+import GenericProductsPage from './components/GenericProductsPage'
 
 class App extends Component {
   constructor() {
-    super();
-    this.updateQuantityInCart = this.updateQuantityInCart.bind(this);
-    this.removeLineItemInCart = this.removeLineItemInCart.bind(this);
-    this.handleCartClose = this.handleCartClose.bind(this);
-    this.handleCartOpen = this.handleCartOpen.bind(this);
+    super()
+    this.updateQuantityInCart = this.updateQuantityInCart.bind(this)
+    this.removeLineItemInCart = this.removeLineItemInCart.bind(this)
+    this.handleCartClose = this.handleCartClose.bind(this)
+    this.handleCartOpen = this.handleCartOpen.bind(this)
   }
   updateQuantityInCart(lineItemId, quantity) {
-    const state = store.getState(); // state from redux store
+    const state = store.getState() // state from redux store
     const checkoutId = state.checkout.id
-    const lineItemsToUpdate = [{id: lineItemId, quantity: parseInt(quantity, 10)}]
-    state.client.checkout.updateLineItems(checkoutId, lineItemsToUpdate).then(res => {
-      store.dispatch({type: 'UPDATE_QUANTITY_IN_CART', payload: {checkout: res}});
-    });
+    const lineItemsToUpdate = [
+      { id: lineItemId, quantity: parseInt(quantity, 10) },
+    ]
+    state.client.checkout
+      .updateLineItems(checkoutId, lineItemsToUpdate)
+      .then(res => {
+        store.dispatch({
+          type: 'UPDATE_QUANTITY_IN_CART',
+          payload: { checkout: res },
+        })
+      })
   }
   removeLineItemInCart(lineItemId) {
-    const state = store.getState(); // state from redux store
+    const state = store.getState() // state from redux store
     const checkoutId = state.checkout.id
-    state.client.checkout.removeLineItems(checkoutId, [lineItemId]).then(res => {
-      store.dispatch({type: 'REMOVE_LINE_ITEM_IN_CART', payload: {checkout: res}});
-    });
+    state.client.checkout
+      .removeLineItems(checkoutId, [lineItemId])
+      .then(res => {
+        store.dispatch({
+          type: 'REMOVE_LINE_ITEM_IN_CART',
+          payload: { checkout: res },
+        })
+      })
   }
   handleCartClose() {
-    store.dispatch({type: 'CLOSE_CART'});
+    store.dispatch({ type: 'CLOSE_CART' })
   }
   handleCartOpen() {
-    store.dispatch({type: 'OPEN_CART'});
+    store.dispatch({ type: 'OPEN_CART' })
   }
   render() {
-    const state = store.getState(); // state from redux store
+    const state = store.getState() // state from redux store
     return (
       <div className="App">
-        <Nav handleCartOpen={this.handleCartOpen}/>
+        <Nav handleCartOpen={this.handleCartOpen} />
         <header className="App-header">
-          <p>
-            Shopify Store dev-store9
-          </p>
+          <p>Shopify Store dev-store9</p>
         </header>
         <Cart
           checkout={state.checkout}
@@ -53,12 +63,12 @@ class App extends Component {
           handleCartClose={this.handleCartClose}
           updateQuantityInCart={this.updateQuantityInCart}
           removeLineItemInCart={this.removeLineItemInCart}
-         />
-       <GenericProductsPage/>
-       <pre>{JSON.stringify(state, null, 2)}</pre>
+        />
+        <GenericProductsPage />
+        {/*        <pre>{JSON.stringify(state, null, 2)}</pre> */}
       </div>
-    );
+    )
   }
 }
 
-export default connect((state) => state)(App);
+export default connect(state => state)(App)
